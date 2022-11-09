363e8615b5f4922b794fd04f85bdbe1d13ecaf3a
Victor Redondo <vrviktor@gmail.com>
2022-11-09 21:36:34 +0100

    Create register new user

--- src/server/controllers/userControllers/userControllers.ts
+++ src/server/controllers/userControllers/userControllers.ts
@@ -1,10 +1,15 @@
 import type { NextFunction, Request, Response } from "express";
 
 import bcrypt from "bcryptjs";
+import CustomError from "../../../CurstomError/CustomError.js";
 import type Credentials from "./types.js";
 import User from "../../../database/models/User/User.js";
 
-export const registerUser = async (req: Request, res: Response) => {
+export const registerUser = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   const { username, password } = req.body as Credentials;
 
   try {
@@ -18,5 +23,12 @@ export const registerUser = async (req: Request, res: Response) => {
     res.status(201).json({
       user: { id: newUser._id, username },
     });
-  } catch (error: unknown) {}
+  } catch (error: unknown) {
+    const customError = new CustomError(
+      (error as Error).message,
+      500,
+      "You couldn't register"
+    );
+    next(customError);
+  }
 };
