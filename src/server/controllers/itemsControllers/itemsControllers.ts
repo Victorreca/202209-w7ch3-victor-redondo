647f7b8a2a901892f9ad892e3c38515fdd641469
Victor Redondo <vrviktor@gmail.com>
2022-11-10 21:30:41 +0100

    Add image to database

--- src/server/controllers/itemsControllers/itemsControllers.ts
+++ src/server/controllers/itemsControllers/itemsControllers.ts
@@ -1,4 +1,6 @@
-import type { Request, Response } from "express";
+import type { Request, RequestHandler, Response } from "express";
+import fs from "fs/promises";
+import path from "path";
 import type { ItemStructure } from "../../../database/models/Item/Item.js";
 import Item from "../../../database/models/Item/Item.js";
 import type { CustomRequest } from "../../types.js";
@@ -11,13 +13,24 @@ export const getItems = async (req: CustomRequest, res: Response) => {
   res.status(200).json({ items });
 };
 
-export const createItems = async (req: Request, res: Response) => {
-  const { madeIn, name } = req.body as ItemStructure;
+export const createItems: RequestHandler = async (
+  req: Request<Record<string, unknown>, Record<string, unknown>, ItemStructure>,
+  res: Response
+) => {
+  const { madeIn, name } = req.body;
+
+  await fs.rename(
+    path.join("assets", "images", req.file.filename),
+    path.join("assets", "images", req.file.originalname)
+  );
 
   const newItem = await Item.create({
     madeIn,
     name,
+    picture: req.file.filename,
   });
 
-  res.status(201).json({ newItem });
+  res
+    .status(201)
+    .json({ newItem, picture: `assets/images/${newItem.picture}` });
 };
--- src/server/routers/itemRouter.ts
+++ src/server/routers/itemRouter.ts
@@ -1,4 +1,6 @@
 import express from "express";
+import multer from "multer";
+import path from "path";
 import {
   createItems,
   getItems,
@@ -6,8 +8,11 @@ import {
 
 // eslint-disable-next-line new-cap
 const itemRouter = express.Router();
+const upload = multer({
+  dest: path.join("assets", "images"),
+});
 
 itemRouter.get("/list", getItems);
-itemRouter.post("/create", createItems);
+itemRouter.post("/create", upload.single("picture"), createItems);
 
 export default itemRouter;
